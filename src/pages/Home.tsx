ed68aa346a8fc42c9930bbe4aec4e0bb4062d800
bruceaudo <audo401@gmail.com>
2023-03-17 20:06:54 +0300

    frontpage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ const Home: React.FC<HomeProps> = () => {
       <header className="border-b border-b-gray-600 py-5 px-2 sticky top-0 z-30 border-l border-l-gray-600 bg-black">
         <p className="font-bold text-2xl flex justify-start">Home</p>
       </header>
+
       <div className="flex gap-2 items-start py-5 px-8 cursor-pointer border-b border-b-gray-600">
         <img className="rounded-full w-12 h-12 overflow-hidden object-cover" src="https://images.unsplash.com/photo-1509967419530-da38b4704bc6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGZhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
 
@@ -23,6 +24,59 @@ const Home: React.FC<HomeProps> = () => {
           </div>
         </form>
       </div>
+
+      <div />
+
+      <div className="flex gap-5 py-5 px-8 items-start cursor-pointer">
+        <img className="rounded-full w-12 h-12 overflow-hidden object-cover" src="https://images.unsplash.com/photo-1509967419530-da38b4704bc6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGZhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+
+        <div>
+          <div className="flex mb-3 items-center justify-between">
+            <div className="flex gap-2 mb-3 items-cente">
+              <p className="font-bold">Bruce Audo</p>
+              <p className="text-gray-200">@audo401</p>
+              <p className="text-gray-200">
+                <span className="mr-2">&middot;</span>2h
+              </p>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 hover:bg-gray-600 rounded-full p-2 hover:text-green-500">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+          </div>
+          <p className="mb-3">This is my first post. I am very excited</p>
+          <div className="rounded-lg overflow-hidden">
+            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGZhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+          </div>
+
+          <div className="flex justify-between items-center mt-2">
+            <p className="flex items-center text-gray-700 ">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 hover:bg-gray-600 rounded-full hover:text-green-500 p-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
+              </svg>
+            </p>
+
+            <p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 hover:bg-gray-600 rounded-full p-2 hover:text-green-500">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+              </svg>
+            </p>
+
+            <p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 hover:bg-gray-600 rounded-full p-2 hover:text-green-500">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25" />
+              </svg>
+            </p>
+
+            <p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 hover:bg-gray-600 rounded-full p-2 hover:text-green-500">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+              </svg>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>;
+
+      
 };
 export default Home;
